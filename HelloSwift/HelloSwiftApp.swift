acd45189283b959ac7b66b0167796ec5049b8570
Anasteisha-Klys <134367935+Anasteisha-Klys@users.noreply.github.com>
2023-05-26 20:10:15 +0700

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by Анастасия Клюшникова on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
